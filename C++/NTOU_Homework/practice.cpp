7c6f17d06446db8b3a057999b1536773e293025c
MickXie <mick.xie.club@gmail.com>
2025-04-08 23:25:46 +0800

    Implement  practice code changes to enhance functionality and improve performance

---
+++ C++/NTOU_Homework/practice.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include "Homework.h"
+using namespace std;
+
+int main()
+{
+    /*
+    !!!Homework1!!!
+    Homework1::datainput data;
+    Homework1::rectangle rect(10.5, 2);
+    cout << "Enter the width and height of the triangle: ";
+    // ignore the newline character
+    cin >> data.x;        // 讀取寬度
+    cin.ignore(100, ','); // 跳過 1 個逗號（如果存在）
+    cin >> data.y;
+    cout << "The width and height of the triangle are: " << data.x << ", " << data.y << endl;
+    Homework1::triangle tri(data.x, data.y);
+    cout << rect.area() << endl;
+    cout << tri.area() << endl;
+    */
+    /*
+    !!!Homework2!!!
+     Homework1::datainput data;
+     cin >> data.x;      // 讀取寬度
+     cin.ignore(1, ','); // 跳過 1 個逗號（如果存在）
+     cin >> data.y;
+     cin.ignore(1, ',');
+     cin >> data.z;
+     Homework2::Clock clock1(data.x, data.y, data.z);
+     clock1.tick();
+     clock1.showtime();
+     system("pause");
+     */
+    /*
+    !!!Homework3!!!
+    Homework3::Animal *pet = new Homework3 ::Animal;
+    pet->makeSound();
+    delete pet;
+    pet = new Homework3::Dog;
+    pet->makeSound();
+    delete pet;
+    pet = new Homework3::Cat;
+    pet->makeSound();
+    delete pet;
+    */
+    /*
+    !!!Homework4!!!
+     Homework1::datainput data;
+     cout << "Enter the car's width, height, and length: " << endl;
+     cin >> data.x;
+     cin.ignore(1, ','); // 跳過 1 個逗號（如果存在）
+     cin >> data.y;
+     cin.ignore(1, ','); // 跳過 1 個逗號（如果存在）
+     cin >> data.z;      // 讀取寬度
+     Homework4::Car car(data.x, data.y, data.z);
+     cout << "The total cost of the car is: " << car.totalCost() << endl;
+     */
+    /*
+    !!!Homework5!!!
+    Homework1::datainput data;
+    cout << "Enter the car'speed limit , car1's speed ,car2's speed" << endl;
+    cin >> data.x;
+    cin.ignore(1, ','); // 跳過 1 個逗號（如果存在）
+    cin >> data.y;
+    cin.ignore(1, ','); // 跳過 1 個逗號（如果存在)
+    cin >> data.z;      // 讀取寬度
+    Homework5::car::getLimit(data.x);
+    Homework5::car car1(data.y);
+    Homework5::car car2(data.z);
+    car1.checkSpeed();
+    car2.checkSpeed();
+    */
+    /*
+    !!!Homework6!!!
+     Homework6::Employees *emp = new Homework6::FullTimeEmployee("Alice", 101, 5000);
+     emp->displayInfo();
+     emp = new Homework6::PartTimeEmployee("Bob", 202, 100, 16);
+     emp->displayInfo();
+     */
+    return 0;
+}
