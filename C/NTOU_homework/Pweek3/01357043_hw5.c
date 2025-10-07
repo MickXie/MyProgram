9dc5c332c11f4300fd1bc9796e047420179f6316
MickXie <mick.xie.club@gmail.com>
2025-10-07 14:30:19 +0800

    update

--- C/NTOU_homework/Pweek3/01357043_hw5.c
+++ C/NTOU_homework/Pweek3/01357043_hw5.c
@@ -7,93 +7,42 @@ typedef struct
     int row;
     int value;
 } term;
-void Sum(poly a[100], int n[100], int t)
+void fast_transpose(term a[], term b[])
 {
-    poly ans = {0};
-    int anst = 0;
-
-    // 收集所有項目
-    for (int i = 0; i < t; i++)
+    int row_terms[a[0].row];
+    int starting_pos[a[0].row];
+    memset(row_terms, 0, sizeof(row_terms));
+    memset(starting_pos, 0, sizeof(starting_pos));
+    b[0].col = a[0].row;
+    b[0].row = a[0].col;
+    b[0].value = a[0].value;
+    for (int i = 1; i <= a[0].value; i++)
+        row_terms[a[i].row]++;
+    for (int i = 1; i < a[0].row; i++)
+        starting_pos[i] = starting_pos[i - 1] + row_terms[i - 1];
+    for (int i = 1; i <= a[0].value; i++)
     {
-        for (int k = 0; k < n[i]; k++)
-        {
-            ans.degree[anst] = a[i].degree[k];
-            ans.coef[anst] = a[i].coef[k];
-            anst++;
-        }
+        int j = starting_pos[a[i].row]++;
+        b[j + 1].col = a[i].row;
+        b[j + 1].row = a[i].col;
+        b[j + 1].value = a[i].value;
     }
-
-    // 依次方由大到小排序 (bubble sort)
-    for (int i = 0; i < anst - 1; i++)
-    {
-        for (int j = i + 1; j < anst; j++)
-        {
-            if (ans.degree[i] < ans.degree[j])
-            {
-                int tmpd = ans.degree[i];
-                ans.degree[i] = ans.degree[j];
-                ans.degree[j] = tmpd;
-
-                int tmpc = ans.coef[i];
-                ans.coef[i] = ans.coef[j];
-                ans.coef[j] = tmpc;
-            }
-        }
-    }
-    poly merged = {0};
-    int mt = 0;
-    for (int i = 0; i < anst; i++)
-    {
-        if (mt > 0 && ans.degree[i] == merged.degree[mt - 1])
-        {
-            // 同次方 -> 係數相加
-            merged.coef[mt - 1] += ans.coef[i];
-        }
-        else
-        {
-            // 新的次方
-            merged.degree[mt] = ans.degree[i];
-            merged.coef[mt] = ans.coef[i];
-            mt++;
-        }
-    }
-
-    // 輸出（排除係數為 0 的項）
-    int count = 0;
-    for (int i = 0; i < mt; i++)
-    {
-        if (merged.coef[i] != 0)
-            count++;
-    }
-    printf("%d\n", count);
-    for (int i = 0; i < mt; i++)
-    {
-        if (merged.coef[i] != 0)
-            printf("%d ", merged.coef[i]);
-    }
-    printf("\n");
-    for (int i = 0; i < mt; i++)
-    {
-        if (merged.coef[i] != 0)
-            printf("%d ", merged.degree[i]);
-    }
-    printf("\n");
 }
-
 int main()
 {
-    term a[MAX_TERMS];
-    int n[100], t = 0;
-
-    while (scanf("%d", &n[t]) != EOF)
+    term a[MAX_TERMS], b[MAX_TERMS];
+    scanf("%d", &a[0].col);
+    scanf("%d", &a[0].row);
+    scanf("%d", &a[0].value);
+    int row_terms[a[0].row];
+    for (int i = 0; i < a[0].value; i++)
     {
-        for (int i = 0; i < n[t]; i++)
-            scanf("%d", &a[t].coef[i]);
-        for (int i = 0; i < n[t]; i++)
-            scanf("%d", &a[t].degree[i]);
-        t++;
+        scanf("%d", &a[i + 1].col);
+        scanf("%d", &a[i + 1].row);
+        scanf("%d", &a[i + 1].value);
     }
-
-    Sum(a, n, t);
+    fast_transpose(a, b);
+    for (int i = 0; i <= b[0].value; i++)
+        printf("%d %d %d\n", b[i].col, b[i].row, b[i].value);
     return 0;
 }
